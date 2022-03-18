3d15d592ecb20b59ba9725367f5d0c23ebfdcd0a
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2022-03-18 09:23:36 -0300

    feat: Reviews and components

---
+++ front-end/ui-app/src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p>Project to learn React and Spring Boot from  16 BITS TECH</p>
+  </div>
+);
+
+export default Footer;
