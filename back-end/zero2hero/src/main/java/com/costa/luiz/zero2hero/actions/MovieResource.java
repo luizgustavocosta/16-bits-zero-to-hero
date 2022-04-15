322d6e6cb30dee5a56d4fb593ae987b547242b28
Luiz Gustavo Costa <luiz.gustavo.costa82@gmail.com>
2022-04-15 19:03:45 -0300

    feat: Swagger documentation

--- back-end/zero2hero/src/main/java/com/costa/luiz/zero2hero/actions/MovieResource.java
+++ back-end/zero2hero/src/main/java/com/costa/luiz/zero2hero/actions/MovieResource.java
@@ -1,13 +1,17 @@
 package com.costa.luiz.zero2hero.actions;
 
-import com.costa.luiz.zero2hero.model.genre.GenreRepository;
 import com.costa.luiz.zero2hero.model.movie.Movie;
 import com.costa.luiz.zero2hero.model.movie.MovieService;
 import com.costa.luiz.zero2hero.model.movie.dto.MovieDto;
 import com.costa.luiz.zero2hero.model.movie.dto.MovieMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,28 +26,32 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name="Movies")
 @RequestMapping("/movies")
 public class MovieResource {
 
     private final MovieService service;
 
-
-    @Autowired
     private final MovieMapper movieMapper;
 
-    @Autowired
-    GenreRepository genreRepository;
-
+    @Operation(summary = "Find movies by user")
     @GetMapping("/{username}/favorites")
     public List<Movie> getAll(@PathVariable String username) {
         throw new UnsupportedOperationException();
     }
 
     @GetMapping
-    public List<MovieDto> getAll() {
-        return service.findAll().stream()
-                .map(movieMapper::toDto)
-                .collect(Collectors.toUnmodifiableList());
+    @Operation(summary = "Return all movies")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
+    public ResponseEntity<List<MovieDto>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.findAll()
+                        .stream()
+                        .map(movieMapper::toDto)
+                        .collect(Collectors.toUnmodifiableList()));
     }
 
     @DeleteMapping(path = "/{id}")
