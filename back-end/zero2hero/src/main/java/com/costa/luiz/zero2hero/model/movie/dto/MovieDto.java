cd1dc03812df2431fdbe6d497e85e6a33f3fc8cd
Luiz Gustavo Costa <luiz.gustavo.costa82@gmail.com>
2022-04-24 19:53:23 -0300

    feat: Getting started with validation

--- back-end/zero2hero/src/main/java/com/costa/luiz/zero2hero/model/movie/dto/MovieDto.java
+++ back-end/zero2hero/src/main/java/com/costa/luiz/zero2hero/model/movie/dto/MovieDto.java
@@ -3,6 +3,11 @@ package com.costa.luiz.zero2hero.model.movie.dto;
 import com.costa.luiz.zero2hero.model.movie.Classification;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,10 +16,20 @@ import java.util.List;
 public class MovieDto {
 
     private Long id;
+    @NotNull
+    @Size(min=2, max=50)
     private String name;
+    @NotNull
+    @Min(1888)
+    @Max(2022)
     private int year;
+    @NotNull
+    @Min(value = 5, message = "Review the movie duration")
+    @Max(value = 240)
     private int duration;
+    @NotEmpty(message = "Please provide a country")
     private String country;
+    @NotEmpty(message = "Please provide a language")
     private String language;
     private Double rating;
     private LocalDateTime lastUpdate;
