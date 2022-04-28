b8dc0dd5c4a8512f299fad5c6224796171b6ef80
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2022-04-27 23:07:04 -0300

    chore: Remove log

--- back-end/zero2hero/src/main/java/com/costa/luiz/zero2hero/Zero2heroApplication.java
+++ back-end/zero2hero/src/main/java/com/costa/luiz/zero2hero/Zero2heroApplication.java
@@ -1,18 +1,15 @@
 package com.costa.luiz.zero2hero;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@Slf4j
 public class Zero2heroApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Zero2heroApplication.class, args);
-        log.debug("Application started");
     }
 
 }
